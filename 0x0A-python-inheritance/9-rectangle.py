61621d8171515d539e3b61f9f53f1342e250e8fd
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2023-12-04 10:05:25 -0800

    9-rectangle.py

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""contain class"""
+
+
+class BaseGeometry:
+    """basegeometry class"""
+    def area(self):
+        """raise exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """integer validator"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <=0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """rectangle class"""
+    def __init__(self, width, height):
+        """initialize"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """string"""
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
