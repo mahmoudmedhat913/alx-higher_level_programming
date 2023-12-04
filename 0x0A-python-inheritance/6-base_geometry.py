e4f31a80b24612323eb8b9e3cb62f216f486a085
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2023-12-04 07:20:27 -0800

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""contain class"""
+
+
+class BaseGeometry:
+    """class contain function"""
+    def area(self):
+        """raise exception"""
+        raise Exception("area() is not implemented")
