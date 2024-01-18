a454e57eae1325ab940769d3c1dc1e68ecedad8c
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-01-17 16:48:40 -0800

    102-rating_shows.sql

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,3 @@
+-- list all shows by rating
+SELECT `title`, SUM(`rate`) AS `rating` FROM `tv_shows` AS t
+	INNER JOIN `tv_show_ratings` AS r ON t.`id` = r.`show_id` GROUP BY `title` ORDER BY `rating` DESC;
