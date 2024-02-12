22dace6d688d1db5f546c9fd334ef0fb3edfbc45
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-02-12 08:22:28 -0800

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,6 +3,6 @@ if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < parseInt(process.argv[2]); i++) {
-	  console.log('C is fun');
+    console.log('C is fun');
   }
 }
