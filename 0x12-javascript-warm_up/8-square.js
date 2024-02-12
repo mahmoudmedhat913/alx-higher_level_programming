211fff4fcbd5581f4fb3c3922c6c2af92591b98f
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-02-12 08:27:37 -0800

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 if (isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < parseInt(process.argv[2]; i++) {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
     console.log('X'.repeat(parseInt(process.argv[2])));
   }
 }
