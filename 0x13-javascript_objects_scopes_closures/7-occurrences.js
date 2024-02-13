c7d73386816ad701becbaac01a0a704ca66b7b85
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-02-13 08:55:18 -0800

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-exports.nb0ccurences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
 };
