eef1e7dd249a0ad782b75116abd31ffb6a06b8dd
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-02-13 08:46:52 -0800

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
-    if (c == indefined) {
+    if (c === indefined) {
       this.print();
     } else {
       for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
