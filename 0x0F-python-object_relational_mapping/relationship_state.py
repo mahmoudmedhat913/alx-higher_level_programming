e919b83d00ea37446ebe3031f931f775a9ddaf55
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-02-16 06:12:46 -0800

    100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -2,6 +2,7 @@
 """contain state class and base"""
 from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
