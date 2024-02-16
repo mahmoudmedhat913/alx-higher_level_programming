8ed340bda4ca07658b4af7c0b0dfa4682b34d2fd
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-02-16 06:15:04 -0800

    100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """start link class"""
 import sys
-from model_state import Base, State
-from model_city import City
-from sqlalchemy import (create_engine)
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
