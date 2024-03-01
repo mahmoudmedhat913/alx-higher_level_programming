2e55f05b5dc304b1c2fc0367b205bf8f1e3a7c23
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-03-01 05:49:24 -0800

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""script that fetches https://intranet.hbtn.io/status"""
+"""script that fetches https://alx-intranet.hbtn.io/status"""
 
 import requests
 
 
-if __name__ == '__main__':
-    response = requests.get('https://intranet.hbtn.io/status')
+if __name__ == "__main__":
+    response = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
