caef45e0e399eba3592760b1d44a8bc05e8a759b
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-03-01 06:20:42 -0800

    10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""python script that takes github credentials"""
+import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/users/{}'.format(argv[1])
+    r = requests.get(url,
+                     auth=HTTPBasicAuth(argv[1], argv[2]))
+    print(r.json().get('id'))
