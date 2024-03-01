415633cf462a2f068293e75f9af5b0be68711b5b
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-03-01 06:12:28 -0800

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""python script that takes in a letter"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    q = argv[1] if len(argv) == 2 else ""
+    url = 'http://0.0.0.0:5000/search-user'
+    r = requests.post(url, data={'q': q})
+    try:
+        r_dict = r.json()
+        id, name = r_dict.get('id'), r_dict.get('name')
+        if len(r_dict) == 0 or not id or not name:
+            print("No result")
+        else:
+            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
+    except Exception:
+        print("Not a valid JSON")
