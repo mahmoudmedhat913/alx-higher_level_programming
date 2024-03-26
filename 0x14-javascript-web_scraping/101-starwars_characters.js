72672410f1551e079d3809ecf17e2d5fd0deecbd
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2024-03-26 07:16:33 -0700

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url , function (error, response, body) {
+request(url, function (error, response, body) {
   if (!error) {
     const characters = JSON.parse(body).characters;
-    printCharacters(characeters, 0);
+    printCharacters(characters, 0);
   }
 });
 
 function printCharacters (characters, index) {
-  request(character[index], function (error, response, body) {
+  request(characters[index], function (error, response, body) {
     if (!error) {
       console.log(JSON.parse(body).name);
       if (index + 1 < characters.length) {
